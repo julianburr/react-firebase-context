4e20d2ee751b07c8b612f49d267f01c5b98a82fa
jobrienski <gymper@gmail.com>
2019-02-19 20:35:40 -0800

    make an hocWithFirestorm that does not require a decorator

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -1,6 +1,13 @@
 import FirestoreContext from './context';
 import FirestoreProvider from './provider';
 import withFirestore from './hoc';
+import hocWithFirestore from './hocWithFirestore';
 import Firestore from './consumer';
 
-export { FirestoreContext, FirestoreProvider, withFirestore, Firestore };
+export {
+  FirestoreContext,
+  FirestoreProvider,
+  withFirestore,
+  hocWithFirestore,
+  Firestore
+};
